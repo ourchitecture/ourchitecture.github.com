1d8b29467708ec706ac58f9e435039b9c47f3469
Eric Swanson <ericis@users.noreply.github.com>
2022-11-04 17:52:11 -0400

    feat: organizational foundations
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

--- src/ionic-ng/src/app/pages/home/home.page.ts
+++ src/ionic-ng/src/app/pages/home/home.page.ts
@@ -1,3 +1,5 @@
+import { DomSanitizer } from '@angular/platform-browser';
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  selectedVideo = null;
+  leavingSite = null;
 
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  openVideo(title, url: string) {
+    this.selectedVideo = {
+      title,
+      url: this.sanitizer.bypassSecurityTrustResourceUrl(url),
+    };
+
+    console.log('open video', this.selectedVideo);
+  }
+
+  closeVideo() {
+    console.log('close video');
+    this.selectedVideo = null;
+  }
+
+  openLeavingSite(url: string) {
+    this.leavingSite = {
+      url,
+    };
+
+    console.log('open leaving site', this.leavingSite);
+  }
+
+  closeLeavingSite() {
+    console.log('close leaving site');
+    this.leavingSite = null;
+  }
 }
