3f43a19be70551d533f1c602194d8c7b75790b05
Eric Swanson <ericis@users.noreply.github.com>
2022-11-10 17:35:34 -0500

    feat: dark mode
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

--- src/ionic-ng/src/app/app.module.ts
+++ src/ionic-ng/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -12,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
+    IonicStorageModule.forRoot(),
     AppRoutingModule,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
--- src/ionic-ng/src/app/pages/home/home.page.ts
+++ src/ionic-ng/src/app/pages/home/home.page.ts
@@ -2,6 +2,10 @@ import { DomSanitizer } from '@angular/platform-browser';
 
 import { Component } from '@angular/core';
 
+import { Storage } from '@ionic/storage-angular';
+
+const STORAGE_KEYS_APP_DARK_MODE = 'app_dark-mode';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -9,10 +13,68 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  wasDarkModeToggleEnabled = true;
+  isDarkModeToggleEnabled = true;
   selectedVideo = null;
   leavingSite = null;
 
-  constructor(private sanitizer: DomSanitizer) {
+  constructor(
+    private sanitizer: DomSanitizer,
+    private storage: Storage) {
+  }
+
+  async ngOnInit() {
+    await this.storage?.create();
+
+    this.wasDarkModeToggleEnabled = await this.getIsDarkModeEnabledFromStorage();
+    this.isDarkModeToggleEnabled = this.wasDarkModeToggleEnabled;
+  }
+
+  async getIsDarkModeEnabledFromStorage() {
+    const storageIsDarkModeEnabled = await this.storage?.get(STORAGE_KEYS_APP_DARK_MODE);
+
+    let isDarkModeEnabled = true;
+
+    // if a preference has never been set, read the system preference
+    if (storageIsDarkModeEnabled == null) {
+      const isDarkModePreferred = window.matchMedia('(prefers-color-scheme: dark)');
+
+      isDarkModeEnabled = isDarkModePreferred ? true : false;
+    } else {
+      isDarkModeEnabled = storageIsDarkModeEnabled ? true : false;
+    }
+
+    document.body.classList.toggle('dark', isDarkModeEnabled);
+
+    return isDarkModeEnabled;
+  }
+
+  // setAppDarkMode() {
+  //   document.body.classList.toggle('dark', this.isDarkModeEnabled);
+  // }
+
+  async toggleDarkMode() {
+    if (!this.storage) {
+      return;
+    }
+
+    // out of sync detection
+    if (this.isDarkModeToggleEnabled != this.wasDarkModeToggleEnabled) {
+      return;
+    }
+
+    // remember the toggled setting
+    this.wasDarkModeToggleEnabled = !this.isDarkModeToggleEnabled;
+
+    // store the toggled setting
+    await this.storage.set(STORAGE_KEYS_APP_DARK_MODE, this.wasDarkModeToggleEnabled);
+
+    console.log('Toggling dark mode setting...');
+
+    document.body.classList.toggle('dark', this.wasDarkModeToggleEnabled);
+
+    // update the toggled setting
+    this.isDarkModeToggleEnabled = this.wasDarkModeToggleEnabled;
   }
 
   openVideo(title, url: string) {
