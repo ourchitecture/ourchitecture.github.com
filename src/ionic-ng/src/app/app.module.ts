2ef2d7c9729076e760cbeae9a696b89d94103a61
Eric Swanson <ericis@users.noreply.github.com>
2022-11-11 19:06:53 -0500

    fix: pipe module use
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

--- src/ionic-ng/src/app/app.module.ts
+++ src/ionic-ng/src/app/app.module.ts
@@ -8,17 +8,15 @@ import { IonicStorageModule } from '@ionic/storage-angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SafePipe } from './pipes/safe.pipe';
 
 @NgModule({
-  declarations: [AppComponent, SafePipe],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     AppRoutingModule,
   ],
-  exports: [SafePipe],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
