97384e586fe967c940c16b9ee51da70928aadd41
Eric Swanson <ericis@users.noreply.github.com>
2022-12-03 10:59:24 -0500

    fix: hello cloud project urls
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

--- src/ionic-ng/src/app/app-routing.module.ts
+++ src/ionic-ng/src/app/app-routing.module.ts
@@ -3,36 +3,48 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'hello-cloud',
+    redirectTo: 'hello-cloud/index.html',
+    pathMatch: 'prefix',
+  },
+  {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomePageModule),
   },
   {
     path: '',
     redirectTo: 'home',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'people',
-    loadChildren: () => import('./pages/people/people.module').then( m => m.PeoplePageModule)
+    loadChildren: () =>
+      import('./pages/people/people.module').then((m) => m.PeoplePageModule),
   },
   {
     path: 'process',
-    loadChildren: () => import('./pages/process/process.module').then( m => m.ProcessPageModule)
+    loadChildren: () =>
+      import('./pages/process/process.module').then((m) => m.ProcessPageModule),
   },
   {
     path: 'value',
-    loadChildren: () => import('./pages/value/value.module').then( m => m.ValuePageModule)
+    loadChildren: () =>
+      import('./pages/value/value.module').then((m) => m.ValuePageModule),
   },
   {
     path: 'technology',
-    loadChildren: () => import('./pages/technology/technology.module').then( m => m.TechnologyPageModule)
+    loadChildren: () =>
+      import('./pages/technology/technology.module').then(
+        (m) => m.TechnologyPageModule
+      ),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
