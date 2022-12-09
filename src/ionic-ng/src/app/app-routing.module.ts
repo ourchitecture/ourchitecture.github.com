d2a1a8b91c1bfa47fa335e92f8078649716f591c
Eric Swanson <ericis@users.noreply.github.com>
2022-12-09 16:38:53 -0500

    chore: remove hello-cloud route
    Signed-off-by: Eric Swanson <ericis@users.noreply.github.com>

--- src/ionic-ng/src/app/app-routing.module.ts
+++ src/ionic-ng/src/app/app-routing.module.ts
@@ -3,11 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'hello-cloud',
-    redirectTo: 'hello-cloud/index.html',
-    pathMatch: 'prefix',
-  },
-  {
     path: 'home',
     loadChildren: () =>
       import('./pages/home/home.module').then((m) => m.HomePageModule),
